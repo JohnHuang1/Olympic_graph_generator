ef480ab9fa6a1eee68245825171b757bdd024066
John Huang <john.jl.huang@gmail.com>
2019-05-22 21:19:47 -0400

    Axis Created

--- Olympic_graph_generator/graphFrm.cs
+++ Olympic_graph_generator/graphFrm.cs
@@ -33,11 +33,10 @@ namespace Olympic_graph_generator
         {
             Label lbl = lblYaxis;
             Font font = lblYaxis.Font;
-            Brush brush = new SolidBrush(Color.Black);
             e.Graphics.TranslateTransform(lbl.Width / 2, lbl.Height / 2);
             e.Graphics.RotateTransform(270);
             SizeF textSize = TextRenderer.MeasureText("Yaxis", font);
-            e.Graphics.DrawString("Yaxis", font, brush, -(textSize.Width / 2), -(textSize.Height / 2));
+            e.Graphics.DrawString("Yaxis", font, new SolidBrush(Color.Black), -(textSize.Width / 2), -(textSize.Height / 2));
             
         }
 
@@ -62,13 +61,29 @@ namespace Olympic_graph_generator
             //Draw Main Line
             canvas.DrawLines(pen, new Point[] { new Point(dist, dist), new Point(dist, height - dist), new Point(width - dist, height - dist) });
 
-            //Draw Y-Axis Increment
+            //Draw Axis Increment
+            //Variables
             int lineSize = 10;
             int numOfInc = 10;
+            int counter = 10;
+            int textBuffer = 2;
+            int incrementAmount = 1;
+            Font font = new Font("Ariel", 10, GraphicsUnit.Pixel);
             for (int i = dist; i <= height - dist; i += (height - dist*2) / numOfInc)
             {
+                //Measure Number
+                SizeF stringSize = TextRenderer.MeasureText(counter.ToString(), font);
+
+                //Y-axis
                 canvas.DrawLine(pen, dist - (lineSize / 2), i, (dist + (lineSize / 2)), i);
+                canvas.DrawString(counter.ToString(), font, new SolidBrush(Color.Black), dist - (lineSize / 2) - stringSize.Width - textBuffer, i - (stringSize.Height / 2) - textBuffer);
+
+                //X-axis
                 canvas.DrawLine(pen, i, height - dist - (lineSize / 2), i, height - dist + (lineSize / 2));
+                canvas.DrawString(counter.ToString(), font, new SolidBrush(Color.Black), width - i - (stringSize.Width / 2) + textBuffer, (height - dist) + (lineSize / 2) + textBuffer);
+
+                //Increment
+                counter -= incrementAmount;
             }
 
         }
